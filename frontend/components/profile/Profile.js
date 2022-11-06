4910a89e75a60ebc296dadd180ba47cd543d84fa
ij03l <idahorjoel@gmail.com>
2022-11-06 17:04:06 +0100

    Made a responsive styling

--- frontend/components/profile/Profile.js
+++ frontend/components/profile/Profile.js
@@ -11,28 +11,28 @@ const Profile = () => {
   const router = useRouter()
   return (
      <div className='grid place-items-center h-screen w-full'>
-            <div className='flex justify-center mb-[67px] w-full '>
-                <div className='bg-[#2D1300] w-[600px] h-[400px] shadow-[0_6px_10px_4px_rgba(0,0,0,0.5)] rounded-[30px] '>
-                <div className='grid place-items-center mt-12 w-full'>
+            <div className='flex justify-center mb-10 w-full '>
+                <div className='bg-[#2D1300] w-1/3 h-90 shadow-[0_6px_10px_4px_rgba(0,0,0,0.5)] rounded-[30px] '>
+                <div className='grid place-items-center mt-6 w-full'>
                     <Image src={ProfileImage} height={150} width={142.67}  />
                     <div className='font-bungee text-[24px] mt-2 text-white'>Amanda Stenberg</div>
                     <div className='font-noto font-semibold text-[#FFFFFF] leading-[14px]  text-[16px]'>{currentAccount.slice(0, 9)}...</div>
                     <div className='flex font-noto font-semibold mt-[10px] ] text-[24px]'>
-                      <div className='text-white'>Host  </div>
-                      <div className='w-[5px] h-[5px] -mt-[2px]'>
-                      <Image src={Ellipse} className='h-[5px] text-[#D9D9D9] w-[5px]' height={5} width={5} />
+                      <div className='text-green-400'>Host</div>
+                      <div className='w-5 pl-2 h-5 -mt-1'>
+                      <Image src={Ellipse}  height={5} width={5} />
                       </div>
-                      <div className='text-white'>  Therapist</div>
+                      <div className='text-white text-10'>Therapist</div>
                     </div>
-                    <div className='font-noto text-[10px] mt-1 text-[#817C7C]'>humanitarian and blablabla</div>
+                    <div className='font-noto text-12 mt-1 text-[#817C7C]'>humanitarian and blablabla</div>
                     <div className='flex items-center w-full justify-center mt-[8px]'>
-                    <div onClick={() => router.push('/call')} className='flex cursor-pointer font-noto rounded-[10px] h-[40px] w-[125px] text-center font-medium bg-[#A77300] mt-[10px] py-2 pl-1 text-[16px]'>
-                    <div className='text-white text-[16px]'>Add Schedule</div>
+                    <div onClick={() => router.push('/call')} className='flex cursor-pointer font-noto rounded-[10px] h-15 w-50 text-center font-medium mb-5 bg-[#A77300] mt-[10px] p-2'>
+                    <div className=' justify-center text-white text-15'>Add Schedule</div>
                     </div>
                    
                   </div>
                   <div className='flex -mt-[40px] mr-12 w-full'>
-                  <div className='text-right text-[#817C7C] text-[14px] ml-auto'>Ratings & Review</div>
+                  <div className='text-right text-[#817C7C] text-[14px] mb-10 ml-auto'><u>Ratings & Reviews</u></div>
                   </div>
                     </div>
                    
