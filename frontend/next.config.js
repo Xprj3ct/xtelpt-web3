6516061b6002d6628ea562e378848b9af344de02
X project <107910326+Xprj3ct@users.noreply.github.com>
2022-11-12 13:27:52 +0100

    replace

--- frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const withTM = require("next-transpile-modules")(["@huddle01/huddle01-client"]);
-
-module.exports = withTM({
-  reactStrictMode: true,
-  swcMinify: true,
-});
